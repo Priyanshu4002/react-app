8803f8c7e2a445b218a8d8cf3976f72da4ea243c
Priyanshu Shrivastava <125429914+Priyanshu4002@users.noreply.github.com>
2023-09-24 21:32:13 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <footer>
+      <div>
+        <h1>Priyanshu</h1>
+        <p>@all right reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+            <a href="https://instagram.com/priyanshu_3._">Instagram</a>
+            <a href="https://github.com/Priyanshu4002">Github</a>
+            <a href="https://www.linkedin.com/in/priyanshu-shrivastava-23170b226">LinkedIn</a>
+        </div>
+      </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
